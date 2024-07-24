e1a9f26767b0e47d5c2d0db0d6aac09177d26419
tonion <thanh26899@gmail.com>
2024-07-25 00:08:28 +0700

    update reserve service

--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -4,12 +4,15 @@ import model.Customer;
 import model.IRoom;
 import model.Reservation;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class ReservationService {
     private static final ReservationService instance = new ReservationService();
+    private static final long RECOMMEND_DATE_RANGE = 7;
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
     public static ReservationService getInstance() {
         return ReservationService.instance;
     }
@@ -44,23 +47,25 @@ public class ReservationService {
     }
 
     public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) {
-        List<IRoom> reservedRooms = new ArrayList<>();
-        for (Reservation reservation : reservations) {
-            reservedRooms.add(reservation.getRoom());
-        }
-        Set<IRoom> availableRoomList = new HashSet<>();
-        for (IRoom room : rooms) {
-            if (!reservedRooms.contains(room)) {
-                availableRoomList.add(room);
-                continue;
+        Collection<IRoom> availableRooms = this.findRoomsInDateRange(checkInDate, checkOutDate);
+        if (availableRooms.isEmpty()) {
+            System.out.print("There is no available room within your inputted date range. ");
+            Date recommendCheckInDate = Date.from(checkInDate.toInstant().plus(RECOMMEND_DATE_RANGE, ChronoUnit.DAYS));
+            Date recommendCheckOutDate = Date.from(checkOutDate.toInstant().plus(RECOMMEND_DATE_RANGE, ChronoUnit.DAYS));
+            availableRooms = this.findRoomsInDateRange(recommendCheckInDate, recommendCheckOutDate);
+            if (availableRooms.isEmpty()) {
+                System.out.println("Also no available room within 7 days extended from your inputted date range. Please try another date range instead.");
+                return new ArrayList<>();
             }
-            for (Reservation reservation : reservations) {
-                if (reservation.getRoom().equals(room) && !isOverlap(checkInDate, checkOutDate, reservation.getCheckInDate(), reservation.getCheckOutDate())) {
-                    availableRoomList.add(room);
-                }
-            }
+            System.out.println("Here are the recommended rooms for you if you reserve from " + DATE_FORMAT.format(recommendCheckInDate)
+                    + " to " + DATE_FORMAT.format(recommendCheckOutDate) + ":");
+
         }
-        return availableRoomList;
+        else {
+            System.out.println("List of rooms are available within the inputted date range: ");
+        }
+        System.out.println(availableRooms);
+        return availableRooms;
     }
 
     public Collection<Reservation> getCustomerReservation(Customer customer) {
@@ -87,5 +92,25 @@ public class ReservationService {
 
     private boolean isOverlap(Date fromDate1, Date toDate1, Date fromDate2, Date toDate2) {
         return !(toDate1.before(fromDate2) || fromDate1.after(toDate2));
+    }
+
+    private Collection<IRoom> findRoomsInDateRange(Date checkInDate, Date checkOutDate) {
+        List<IRoom> reservedRooms = new ArrayList<>();
+        for (Reservation reservation : reservations) {
+            reservedRooms.add(reservation.getRoom());
+        }
+        Set<IRoom> availableRoomList = new HashSet<>();
+        for (IRoom room : rooms) {
+            if (!reservedRooms.contains(room)) {
+                availableRoomList.add(room);
+                continue;
+            }
+            for (Reservation reservation : reservations) {
+                if (reservation.getRoom().equals(room) && !isOverlap(checkInDate, checkOutDate, reservation.getCheckInDate(), reservation.getCheckOutDate())) {
+                    availableRoomList.add(room);
+                }
+            }
+        }
+        return availableRoomList;
     }
 }
